fd8b0d86b4d64d81c789c6254379ece47231b43b
mohamedhendy <mohamadhendy14@gmail.com>
2021-01-27 02:27:00 +0200

    Updates

---
+++ precache-manifest.a3d919b8d96a9f4e8aa8b6e40959fd02.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001246f4a3fb4172b5fd69f9861d6545",
+    "url": "/mohamedhendy/saadko/index.html"
+  },
+  {
+    "revision": "a75567334e8db8e71511",
+    "url": "/mohamedhendy/saadko/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b7310a9c4f00c63f5472",
+    "url": "/mohamedhendy/saadko/static/css/main.33c2672c.chunk.css"
+  },
+  {
+    "revision": "a75567334e8db8e71511",
+    "url": "/mohamedhendy/saadko/static/js/2.1ff64019.chunk.js"
+  },
+  {
+    "revision": "fe4c430e79721ece8ae4ffc1ec87db19",
+    "url": "/mohamedhendy/saadko/static/js/2.1ff64019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7310a9c4f00c63f5472",
+    "url": "/mohamedhendy/saadko/static/js/main.163d17e8.chunk.js"
+  },
+  {
+    "revision": "77755a5affa79e8ef40d",
+    "url": "/mohamedhendy/saadko/static/js/runtime-main.0a5a405a.js"
+  },
+  {
+    "revision": "eb7333c7a830a57517a739a509526502",
+    "url": "/mohamedhendy/saadko/static/media/about.eb7333c7.png"
+  },
+  {
+    "revision": "6192a74a33ebeac8c31f51955054ae56",
+    "url": "/mohamedhendy/saadko/static/media/ahmed.6192a74a.png"
+  },
+  {
+    "revision": "29c52f5b5213f179e5242ebea3e477e0",
+    "url": "/mohamedhendy/saadko/static/media/build.29c52f5b.png"
+  },
+  {
+    "revision": "3ddffed07c34f47bcdd98ce2d537f1d9",
+    "url": "/mohamedhendy/saadko/static/media/build2.3ddffed0.png"
+  },
+  {
+    "revision": "f0376b1190f0bd6b2625eff4e7340ede",
+    "url": "/mohamedhendy/saadko/static/media/business.f0376b11.png"
+  },
+  {
+    "revision": "d40b9bbe0646c187a53643b677fe5b45",
+    "url": "/mohamedhendy/saadko/static/media/charts.d40b9bbe.png"
+  },
+  {
+    "revision": "73b6c6091a0916d8a6c5aed315393483",
+    "url": "/mohamedhendy/saadko/static/media/client1.73b6c609.png"
+  },
+  {
+    "revision": "7baaf94b6bca8c43f704ae33430bfdd0",
+    "url": "/mohamedhendy/saadko/static/media/client2.7baaf94b.png"
+  },
+  {
+    "revision": "cee960cf27d1f060319e7f5e0a3b9a15",
+    "url": "/mohamedhendy/saadko/static/media/client3.cee960cf.png"
+  },
+  {
+    "revision": "a396cf0a3acdda42548ebda5071812d9",
+    "url": "/mohamedhendy/saadko/static/media/company.a396cf0a.png"
+  },
+  {
+    "revision": "407e5b3755af7a038b0a713956f847ea",
+    "url": "/mohamedhendy/saadko/static/media/construction.407e5b37.png"
+  },
+  {
+    "revision": "da6377a75c70a6d9801aa2e167a144c0",
+    "url": "/mohamedhendy/saadko/static/media/construction2.da6377a7.png"
+  },
+  {
+    "revision": "f09222966e3ab2f0eb2185d2a4583de0",
+    "url": "/mohamedhendy/saadko/static/media/factory.f0922296.png"
+  },
+  {
+    "revision": "9c14717895db86f053a611453bd7ea3f",
+    "url": "/mohamedhendy/saadko/static/media/glass1.9c147178.png"
+  },
+  {
+    "revision": "15b28ec83314645ba76da2ccb4833379",
+    "url": "/mohamedhendy/saadko/static/media/glass2.15b28ec8.png"
+  },
+  {
+    "revision": "f3f0d0197087a640904dd2867b00cf6c",
+    "url": "/mohamedhendy/saadko/static/media/glass3.f3f0d019.png"
+  },
+  {
+    "revision": "c44eb64f7f8e747ffe3a8a76485684b6",
+    "url": "/mohamedhendy/saadko/static/media/glass4.c44eb64f.png"
+  },
+  {
+    "revision": "5619f6a153f0bc06612cd936c9d0c86e",
+    "url": "/mohamedhendy/saadko/static/media/img1.5619f6a1.png"
+  },
+  {
+    "revision": "d8052438139c13956d7d2906ab4269d4",
+    "url": "/mohamedhendy/saadko/static/media/img2.d8052438.png"
+  },
+  {
+    "revision": "aefe45cb0eaf0f77bfccc4c0540b3a90",
+    "url": "/mohamedhendy/saadko/static/media/img3.aefe45cb.png"
+  },
+  {
+    "revision": "ed76027ba984ca754ad7edbf40c509c7",
+    "url": "/mohamedhendy/saadko/static/media/img4.ed76027b.png"
+  },
+  {
+    "revision": "fa49bf6ceedf6ba7a500a756abdfa39d",
+    "url": "/mohamedhendy/saadko/static/media/img5.fa49bf6c.png"
+  },
+  {
+    "revision": "4100f41e56d5d61157e8f855b32f880a",
+    "url": "/mohamedhendy/saadko/static/media/img6.4100f41e.png"
+  },
+  {
+    "revision": "d58f819228f372547e56674f9b46564a",
+    "url": "/mohamedhendy/saadko/static/media/img7.d58f8192.png"
+  },
+  {
+    "revision": "6febd896ef0f6be5c26a734ce6f5df19",
+    "url": "/mohamedhendy/saadko/static/media/img8.6febd896.png"
+  },
+  {
+    "revision": "8b37900a16237b66b9f627cd1a14c5df",
+    "url": "/mohamedhendy/saadko/static/media/img9.8b37900a.png"
+  },
+  {
+    "revision": "f52b16aa5c49c87fa68cbc2818360807",
+    "url": "/mohamedhendy/saadko/static/media/kamal.f52b16aa.png"
+  },
+  {
+    "revision": "015e3495aefa40bd5a15f22ba0dcaac4",
+    "url": "/mohamedhendy/saadko/static/media/logo.015e3495.png"
+  },
+  {
+    "revision": "9ed671375b18d6ee0890c38def2a62f6",
+    "url": "/mohamedhendy/saadko/static/media/main-company.9ed67137.png"
+  },
+  {
+    "revision": "97c7528efd05fae3973778af88e60d7e",
+    "url": "/mohamedhendy/saadko/static/media/mental.97c7528e.png"
+  },
+  {
+    "revision": "e6fc1bb41b2b245daff3f49c285f226b",
+    "url": "/mohamedhendy/saadko/static/media/sayed.e6fc1bb4.png"
+  },
+  {
+    "revision": "26133d764019a58ab29010dd3f5d151e",
+    "url": "/mohamedhendy/saadko/static/media/school.26133d76.png"
+  },
+  {
+    "revision": "2533ec129270dfb8821a06fff75ef31f",
+    "url": "/mohamedhendy/saadko/static/media/support.2533ec12.png"
+  },
+  {
+    "revision": "d51fa181c995690a13d3f0301db364a7",
+    "url": "/mohamedhendy/saadko/static/media/tech.d51fa181.png"
+  },
+  {
+    "revision": "7ce238d8cbdc0b0655c6ca58c750583d",
+    "url": "/mohamedhendy/saadko/static/media/welding.7ce238d8.png"
+  },
+  {
+    "revision": "2cd5a67d004783caf7c17474cfe479ad",
+    "url": "/mohamedhendy/saadko/static/media/work.2cd5a67d.mp4"
+  }
+]);
